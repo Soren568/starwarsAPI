cddaec2d76ae05ac69688fb277b79a372d8e43b7
Soren <85842462+Soren568@users.noreply.github.com>
2021-10-28 13:04:07 -0600

    bug fix

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import droid from '../icons/droid.png';
 
 const Filter = props => {
     const[apiId, setApiId] = useState("");
@@ -11,11 +12,15 @@ const Filter = props => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(apiId === "" || apiId < 0) return;
-        axios.get(`https://swapi.dev/api/${category}/${apiId}`).then(resp => data.setData(resp.data))
-        history.push(`/${category}/${apiId}`)
-        setFilter(category);
-        setApiId("")
+        if(apiId === "" || apiId < 0 || category === 'fail' || category === "") {
+            setFilter('fail');
+        } else {
+            setFilter(category);
+            console.log("cat: " + category + "| api: " + apiId)
+            axios.get(`https://swapi.dev/api/${category}/${apiId}`).then(resp => data.setData(resp.data))
+            history.push(`/${category}/${apiId}`)
+            setApiId("")
+        }
     }
     const handleCancel = (e) => {
         e.preventDefault();
@@ -26,7 +31,7 @@ const Filter = props => {
         if(filter === "people"){
             return (
                 <>
-                    <h1>Name: {data.data.name}</h1>
+                    <h1 className="mt-6 text-blue-800 font-bold text-lg">Name: {data.data.name}</h1>
                     <p>Height: {data.data.height} cm</p>
                     <p>Mass: {data.data.mass}</p>
                     <p>Hair Color: {data.data.hair_color}</p>
@@ -36,11 +41,19 @@ const Filter = props => {
         } else if(filter === "planets"){
             return (
                 <>
-                    <h1>Name: {data.data.name}</h1>
+                    <h1 className="mt-6 text-green-800 font-bold text-lg">Name: {data.data.name}</h1>
                     <h1>Climate: {data.data.climate}</h1>
                     <p>Terrain: {data.data.terrain} cm</p>
                     <p>Surface Water: {data.data.surface_water}</p>
                     <p>Population: {data.data.population}</p>
+                </>
+            )
+        } else if(filter === "fail"){
+            console.log(`%c ERROR: Bad request`, "color:red ; font-size: 15px" );
+            return (
+                <>
+                    <h1 className="mt-6 text-gray-400 font-bold text-lg">These are not the droids you're looking for</h1>
+                    <img src={droid} alt="droid" width="400" />
                 </>
             )
         } else {
@@ -53,7 +66,7 @@ const Filter = props => {
         <form className="max-w-sm" onSubmit={handleSubmit}>
             <div className="flex items-center border-b border-yellow-500 py-2 bg-gray-50 rounded p-2">
                 <select onChange={e => setCategory(e.target.value)} value={category} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none border-2 border-gray-200 bg-gray-200 rounded" name="" id="">
-                    <option value="n/a" className="text-gray-600" defaultValue>Select an option </option>
+                    <option value="fail" className="text-gray-600" defaultValue>Select an option </option>
                     <option value="people">Person</option>
                     <option value="planets">World</option>
                 </select>
